c5731186cf1075a1556edc633970b91b12aaddcd
rehan <rehanrehman389@gmail.com>
2024-04-28 12:25:10 +0530

    v-once and v-pre

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,8 @@
 <template>
-  <div>
-    <pre>{{ formValues }}</pre>
-  </div>
-  <form @submit.prevent="submitForm">
-    <div>
-      <label for="name">Name</label>
-      <input type="text" id="name" v-model.trim.lazy="formValues.name">
-    </div>
-    <div>
-      <label for="age">Age</label>
-      <input @keyup.enter="submitForm" type="number" id="age" v-model="formValues.age">
-    </div>
-    <div>
-      <label for="profile">Profile</label>
-      <textarea id="profile" v-model="formValues.profileSummary" />
-    </div>
-    <div>
-      <label for="country">Country</label>
-      <select id="country" v-model="formValues.country">
-        <option value="">Select a country</option>
-        <option value="India">India</option>
-        <option value="singapore">Singapore</option>
-      </select>
-    </div>
-    <div>
-      <label for="skills">Skills</label>
-      <select id="skills" multiple v-model="formValues.skills">
-        <option value="java">JAVA</option>
-        <option value="sql">SQL</option>
-      </select>
-    </div>
-    <div>
-      <input type="checkbox" id="remotework" v-model="formValues.remoteWork" true-value="yes" false-value="no" />
-      <label for="remotework">Open to remote?</label>
-    </div>
+  <h2 v-once>{{ name }}</h2>
+  <button @click="name = 'Midhat'">Change Name</button>
 
-    <!-- <div><button>Submit</button></div> -->
-  </form>
+  <h2 v-pre>{{ name }}</h2>
 </template>
 
 <script>
@@ -45,20 +11,11 @@ export default {
   name: 'App',
   data() {
     return {
-      formValues: {
-        name: '',
-        profileSummary: '',
-        country: '',
-        skills: [],
-        remoteWork: "no",
-        age: null
-      }
+      name: 'Rehan'
     }
   },
   methods: {
-    submitForm() {
-      console.log('Form Values', this.formValues)
-    }
+
   }
 }
 </script>
