9b28590150a799711914cfd3698af2fd76b8a747
rehan <rehanrehman389@gmail.com>
2024-04-29 20:23:21 +0530

    How to call components in Vue Home file and extension for vscode

--- src/App.vue
+++ src/App.vue
@@ -1,63 +1,14 @@
 <template>
-  <h2>Volume Tracker (0-20)</h2>
-  <h3>Current Volume - {{ volume }}</h3>
-  <div>
-    <button @click="volume += 2">Increase</button>
-    <button @click="volume -= 2">Decrease</button>
-  </div>
-  <input type="text" v-model="movie">
-
-  <input type="text" v-model="movieInfo.title">
-  <input type="text" v-model="movieInfo.actor">
-
-  <div>
-    <button @click="movieList = movieList.concat(['Wonder Women'])">Add</button>
-  </div>
+  <GreetText />
 </template>
 
 <script>
+import GreetText from './components/GreetText.vue'
 
 export default {
   name: 'App',
-  data() {
-    return {
-      volume: 0,
-      movie: 'Rehan',
-      movieInfo: {
-        title: '',
-        actor: ''
-      },
-      movieList: ['Batman', 'Superman']
-    }
-  },
-  methods: {
-  },
-  computed: {
-  },
-  watch: {
-    volume(newValue, oldValue) {
-      if(newValue > oldValue && newValue === 16){
-        alert('high volume')
-      }
-    },
-    movie: {
-      handler(newValue){
-        console.log(`Calling API with Moviename = ${newValue}`)
-      },
-      immediate: true,
-    },
-    movieInfo: {
-      handler(newValue){
-        console.log(`calling title = ${newValue.title} and actor = ${newValue.actor}`)
-      },
-      deep: true
-    },
-    movieList: {
-      handler(newValue){
-        console.log(`Updated List ${newValue}`)
-      },
-      // deep: true
-    }
+  components: {
+    GreetText
   }
 }
 </script>
