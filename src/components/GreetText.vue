23f76ffc15986c74a6d0084410f67b08ed9824a4
rehan <rehanrehman389@gmail.com>
2024-05-06 22:39:48 +0530

    how to use reactive

--- src/components/GreetText.vue
+++ src/components/GreetText.vue
@@ -1,24 +1,39 @@
 <template>
-    <h2>hello {{ t_name }} a.k.a {{ heroname }}</h2>
+    <h2>hello {{ t_name }} user {{ t_username }} a.k.a {{ t_heroname }}</h2>
+    <h2>{{ greet }}</h2>
+
+    <h2>hello {{ state.t_name }} user {{ state.t_username }} a.k.a {{ state.t_heroname }}</h2>
+    <h2>{{ newgreet }}</h2>
 </template>
 
 <script>
-import { ref } from 'vue'
+import { ref, reactive } from 'vue'
 export default {
     name: 'GreetText',
     setup() {
         const t_name = ref('Ansari')
+        const t_username = ref('ansariofficial')
+        const t_heroname = ref('TTT')
         // t_name.value //access value
 
+        const greet = `Hello ${t_name.value} of user ${t_username.value} aka ${t_heroname.value}`
+
+        const state = reactive({
+            t_name: 'Ayesha',
+            t_username: 'midhat__shaikh',
+            t_heroname: 'TTT'
+        })
+
+        const newgreet = `Hello ${state.t_name} of user ${state.t_username} aka ${state.t_heroname}`
+
         return {
-            t_name
+            t_name,
+            t_username,
+            t_heroname,
+            greet,
+            newgreet,
+            state
         }
     },
-    data() {
-        return{
-            name: 'Rehan',
-            heroname: 'TTT'
-        }
-    }
 }
 </script>
