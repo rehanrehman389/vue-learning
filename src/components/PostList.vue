31177b6e5be11a478fe9a35b159788f6debff886
rehan <rehanrehman389@gmail.com>
2024-05-06 16:13:54 +0530

    created hook

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -13,6 +13,9 @@
 import axios from 'axios'
     export default {
         name: 'PostList',
+        created() {
+            this.getPosts()
+        },
         data() {
             return {
                 posts: [],
