3cc5ff5b7d5f699e57bcbff65802036950043478
rehan <rehanrehman389@gmail.com>
2024-05-07 14:11:07 +0530

    computed in composition API

--- src/components/MethodS.vue
+++ src/components/MethodS.vue
@@ -2,27 +2,47 @@
     <div>
         <input type="text" placeholder="First Name" v-model="fName" />
         <input type="text" placeholder="Last Name" v-model="lName" />
+        <h2>{{ fullName }}</h2>
 
-        <input type="text" placeholder="Hero Name" v-model="heroName" />
-        <input type="text" placeholder="First Name" v-model="firstName" />
-        <input type="text" placeholder="Last Name" v-model="lastName" />
+
+        <input type="text" placeholder="First Name" v-model="refFirstName" />
+        <input type="text" placeholder="Last Name" v-model="refLastName" />
+        <h2>{{ refFullName }}</h2>
+
+        <input type="text" placeholder="First Name" v-model="reactiveFirstName" />
+        <input type="text" placeholder="Last Name" v-model="reactiveLastName" />
+        <h2>{{ reactiveFullName }}</h2>
     </div>
 </template>
 
 <script>
-import { ref, reactive, toRefs } from 'vue'
+import { ref, computed, reactive, toRefs } from 'vue'
     export default {
         name: 'MethodS',
         setup() {
-            const heroName = ref('')
+            const refFirstName = ref('')
+            const refLastName = ref('')
+
+            const refFullName = computed(function() {
+                return `${refFirstName.value} ${refLastName.value}`
+            })
+
+            const reactiveFullName = computed(function() {
+                return `${state.reactiveFirstName} ${state.reactiveLastName}`
+            })
+
+            // const heroName = ref('')
             const state = reactive({
-                firstName: '',
-                lastName: ''
+                reactiveFirstName: '',
+                reactiveLastName: ''
             })
 
             return{
-                heroName,
-                ...toRefs(state)
+                refFirstName,
+                refLastName,
+                refFullName,
+                ...toRefs(state),
+                reactiveFullName
             }
         },
         data() {
@@ -30,7 +50,12 @@ import { ref, reactive, toRefs } from 'vue'
                 fName: '',
                 lName: ''
             }
-        }
+        },
+        computed: {
+            fullName() {
+                return `${this.fName} ${this.lName}`
+            },
+        },
     }
 </script>
 
